fbcc26ec08d7fdfb115a235dd01b6e71280e692f
franciscotudel-overtel <74053305+franciscotudel-overtel@users.noreply.github.com>
2020-11-26 17:05:38 +0100

    Create only_free_percent_plus_size_on_label.cmd

---
+++ plugins/only_free_percent_plus_size_on_label.cmd
@@ -0,0 +1,4 @@
+@REM Solo modulo con % libre de los discos y el size total en la etiqueta de los modulos
+@rem @cscript.exe //B ".\.vbs" //nologo
+@ECHO OFF
+@cscript.exe //B ".\disk_usage_with_total.vbs" //nologo
